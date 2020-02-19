88940158aced4aa921a76e0d0cba9e24ca429781
JoshWinebrener <joshua.winebrener@gmail.com>
2020-02-19 11:18:10 -0600

    Corrected xyz axis error and finished input method

--- cube.py
+++ cube.py
@@ -11,15 +11,15 @@ class Cube:
                 self.faces.append(Face(x, y, 2, 'w'))
                 self.faces.append(Face(x, y, -2, 'y'))
         # populate red and orange faces
-        for x in range(-1, 2):
-            for z in range(-1, 2):
-                self.faces.append(Face(x, 2, z, 'r'))
-                self.faces.append(Face(x, -2, z, 'o'))
-        # populate blue and green faces
         for y in range(-1, 2):
             for z in range(-1, 2):
-                self.faces.append(Face(2, y, z, 'b'))
-                self.faces.append(Face(-2, y, z, 'g'))
+                self.faces.append(Face(2, y, z, 'r'))
+                self.faces.append(Face(-2, y, z, 'o'))
+        # populate blue and green faces
+        for x in range(-1, 2):
+            for z in range(-1, 2):
+                self.faces.append(Face(x, 2, z, 'b'))
+                self.faces.append(Face(x, -2, z, 'g'))
         return
 
     # Side is int for random generation.
@@ -94,29 +94,7 @@ class Cube:
     def input(self):
         print('Type in the color of the stickers around each center piece:')
         print('w for white, r for red, b for blue, o for orange, g for green, and y for yellow')
-        print('type q to quit')
-        # j = 0
-        # for i in range(9):
-        #     if i <3 or i >= 6:
-        #         print('-  -  -  {0:2d} {1:2d} {2:2d}  -  -  -  -  -  -'.format(j, j+1, j+2))
-        #         j += 3
-        #     elif i < 6:
-        #         print('{0:2d} {1:2d} {2:2d} {3:2d} {4:2d} {5:2d} {6:2d} {7:2d} {8:2d} {9:2d} {10:2d} {11:2d}'.format(j, j+1, j+2, j+3, j+4, j+5, j+6, j+7, j+8, j+9, j+10, j+11))
-        #         j += 9
-        # faces = []
-        # i = 0
-        # while i < 44:
-            # face = input('face {0}: '.format(i))
-            # if face == 'w' or face == 'r' or face == 'b' or face == 'o' or face == 'g' or face == 'y':
-            #     faces.insert(i, face)
-            # elif face == 'q':
-            #     print('quitting cube input...\n')
-            #     faces = []
-            #     break
-            # else:
-            #     print('not a valid color.  Make sure that the input character was w, r, b, o, g, or y.\nTrying again...')
-            #     i -= 1
-            # i += 1
+        print('type q to quit, u to undo')
 
         # Top face:
         print('-  -  -  1  2  3  -  -  -  -  -  -')
@@ -125,42 +103,82 @@ class Cube:
         print('9  10 11 17 18 19 25 26 27 33 34 35')
         print('12 g  13 20 r  21 28 b  29 36 o  37')
         print('14 15 16 22 23 24 30 31 32 38 39 40')
-        print('-  -  - 41 42 43  -  -  -  -  -  -')
-        print('-  -  - 44 y  45  -  -  -  -  -  -')
-        print('-  -  - 46 47 48  -  -  -  -  -  -')
+        print('-  -  -  41 42 43  -  -  -  -  -  -')
+        print('-  -  -  44 y  45  -  -  -  -  -  -')
+        print('-  -  -  46 47 48  -  -  -  -  -  -')
 
         colors = []
 
         # Collect colors from user
-        for i in range(48):
+        i = 0
+        while i < 48:
             color = input('face {0}: '.format(i + 1))
             if color == 'w' or color == 'r' or color == 'b' or color == 'o' or color == 'g' or color == 'y':
                 colors.insert(i, color)
             elif color == 'q':
                 print('quitting cube input...\n')
-                faces = []
                 break
+            elif color == 'u' and i > 1:
+                print('undoing last entry...')
+                i -= 2
             else:
                 print('not a valid color.  Make sure that the input character was w, r, b, o, g, or y.\nTrying again...')
                 i -= 1
             i += 1
+            if i/8 == 1: print("\n\nWhite side input complete!\n")
+            elif i/8 == 2: print("\n\nGreen side input complete!\n")
+            elif i/8 == 3: print("\n\nRed side input complete!\n")
+            elif i/8 == 4: print("\n\nBlue side input complete!\n")
+            elif i/8 == 5: print("\n\nOrange side input complete!\n")
+            elif i/8 == 6: print("\n\nYellow side input complete!\n")
         
+        print("**************************************************************************************************\n")
+        
+        self.faces = []
+
         # Arrange colors as faces
         for i in range(48):
+            index = i # Stores which of the 9 faces (including the center) our current face is
+            index %= 8
+            # Compensate for the missing center piece
+            if index > 3:
+                index += 1
+
             face = math.floor(i / 8)
-            # TODO: add 2nd and 3rd dimension with mod function
             if face == 0: # White face
                 z = 2
+                x = index % 3 - 1 # Mod left to right
+                y = math.floor(index / 3) - 1 # Division top to bottom
             elif face == 1: # Green face
                 y = -2
+                x = index % 3 - 1
+                z = 1 - math.floor(index / 3)
             elif face == 2: # Red face
                 x = 2
+                y = index % 3 - 1
+                z = 1 - math.floor(index / 3)
             elif face == 3: # Blue face
                 y = 2
+                x = 1 - (index % 3)
+                z = 1 - math.floor(index / 3)
             elif face == 4: # Orange face
                 x = -2
+                y = 1 - (index % 3)
+                z = 1 - math.floor(index / 3)
             elif face == 5: # Yellow face
                 z = -2
+                x = index % 3 - 1
+                y = 1 - math.floor(index / 3)
+                
+            self.faces.append(Face(x, y, z, colors[i]))
+        
+        # Add the static center faces
+        self.faces.append(Face(0, 0, 2, 'w'))
+        self.faces.append(Face(0, -2, 0, 'g'))
+        self.faces.append(Face(2, 0, 0, 'r'))
+        self.faces.append(Face(0, 2, 0, 'b'))
+        self.faces.append(Face(-2, 0, 0, 'o'))
+        self.faces.append(Face(0, 0, -2, 'y'))
 
 
     
@@ -169,9 +187,9 @@ class Cube:
         # the faces, with spaces where there is no face.
         _colors = []
         for y in range(9):
-            _colors.insert(y, [])
+            _colors.append([])
             for x in range(12):
-                _colors[y].insert(x, ' ')
+                _colors[y].append("-")
 
         for face in self.faces:
             # insert top faces
@@ -180,23 +198,22 @@ class Cube:
             # insert bottom faces
             elif face.z == -2:
                 _colors[-face.y + 7][face.x + 4] = face.color
-            # insert front faces
-            elif face.y == 2:
-                _colors[-face.z + 4][face.x + 4] = face.color
-            # insert back faces
-            elif face.y == -2:
-                _colors[-face.z + 4][-face.x + 10] = face.color
-            # insert right faces
             elif face.x == 2:
-                _colors[-face.z + 4][-face.y + 7] = face.color
-            # insert left faces
+                _colors[-face.z + 4][face.y + 4] = face.color
+            # insert back faces
             elif face.x == -2:
-                _colors[-face.z + 4][face.y + 1] = face.color
+                _colors[-face.z + 4][-face.y + 10] = face.color
+            # insert right faces
+            elif face.y == 2:
+                _colors[-face.z + 4][-face.x + 7] = face.color
+            # insert left faces
+            elif face.y == -2:
+                _colors[-face.z + 4][face.x + 1] = face.color
         
-        print('-------------------------')
+        print(' ----------------------- ')
         for y in range(9):
             print('|' + ' '.join(_colors[y]) + '|')
-        print('-------------------------')
+        print(' ----------------------- ')
         return
     
     def scramble(self):
